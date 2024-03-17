3cf66d378a0f674e0f6ec84d047b1898e7be9dc5
dasilvacsv <dasilva.csv@gmail.com>
2024-03-17 11:29:46 -0400

    first commit

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+export const FRONTEND_URL = process.env.FRONTEND_URL 
+|| "http://localhost:5173"
+
+
+export const DB_HOST = process.env.DB_HOST || "localhost"
+export const DB_PORT = process.env.DB_PORT || 5432
+export const DB_DATABASE = process.env.DB_DATABASE || "expenses"
+export const DB_USER = process.env.DB_USER || "postgres"
+export const DB_PASSWORD = process.env.DB_PASSWORD || "password"
+
+export const PORT = process.env.PORT || 3000
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, FRONTEND_URL, PORT } from "./config.js";
+
+const pool = new pg.Pool({
+  host: DB_HOST,
+  database: DB_DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
+const app = express();
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+);
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW ()");
+  res.send({
+    pong: result.rows[0].now,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
